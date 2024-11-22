0e207c64dd26f60346152093ded1c991089723de
Raihan Nismara <raihannismara@gmail.com>
2024-11-22 07:27:15 +0700

    ubah folder files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { readFileSync } from "fs";
 import path from "path";
 
 export default function handler(req, res) {
-  const file = path.join(process.cwd(), "files", "db.json");
+  const file = path.join(process.cwd(), "db.json");
   const stringfied = readFileSync(file, "utf8");
 
   res.setHeader("Content-Type", "application/json");
